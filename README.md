336a1645e2503dc77cecaf02698318b067519251
DN-SCRIPT <143013550+DN-SCRIPT@users.noreply.github.com>
2023-08-31 16:46:36 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+:wave: Hey WELCOME TO DN DISCORD PRESENCE SHOWCASE
+
+A SCRIPT THAT ALLOWS YOUR PLAYING SERVER STATUS .. !!
+
+# Features :star:
+
+   [ FREE ]
+   
+>. 2 Linkable Buttons
+
+>. Customizable
+
+>. Can add AppBigAssetID and  AppSmallAssetID
+
+>. Playtime
+
+>. show PLAYER ID , PLAYER NAME , PLAYERS
+
+>. Update every 1 mins 
+
+
+# Download & Installation
+
+Download - DN DISCORD PRESENCE
+
+DRAG TO FILE
+
+ENSURE 
+
+# IMAGE :star:
+https://cdn.discordapp.com/attachments/1141381331750752347/1146749443413119006/Screenshot_2023-08-30_180251.png
---
+++ shared/config.lua
@@ -0,0 +1,14 @@
+Config = {
+    DiscordAppID = 1104297347950854144, -- discord bot app id https://discord.com/developers/docs/intro
+    AppBigAssetID = 'dn_development',
+    AppBigAssetText = 'dn_development',
+    AppSmallAssetID = 'dn_development',
+    AppSmallAssetText = 'HELLO GUYS',
+    FirstButtonPlaceholder = 'DN SCRIPTS',
+    FirstButtonLink = 'https://discord.gg/nhE38dbX7Z',
+    SecondButtonPlaceholder = 'Discord',
+    SecondButtonLink = 'https://discord.gg/nhE38dbX7Z',
+
+    UpdateEvery = 60000, -- In ms. (60000ms = 1 minute)
+    Template = '{Citizens} | ID: {1D} | NAME: {NAME}' -- {NAME}, {ID}, {Citizens}
+}
